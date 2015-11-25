9086e5be767216e2dbcec6e76dbb3787a25a78c4
hasulica <raduasandei@gmail.com>
2015-11-25 11:02:37 +0000

    Added validations

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,11 +1,10 @@
 require 'spec_helper'
 
 describe Restaurant, type: :model do
-  it{ is_expected.to have_many :reviews }
-
-  it 'is not valid with a name less than three characters' do
-    restaurant = Restaurant.new(name: 'kf')
-    expect(restaurant).to have(1).error_on(:name)
-    expect(restaurant).not_to be_valid
+  it { is_expected.to have_many :reviews }
+  it 'is not valid unless it has a unique name' do
+    Restaurant.create(name: 'Five Guys')
+    restaurant = Restaurant.new(name: 'Five Guys')
+    expect(restaurant).to have(1).error_on :name
   end
 end
