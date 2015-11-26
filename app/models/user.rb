33dadc77b5a184928a0028b846549c67feb3279a
Ivan Sathianathan <ivan.sathianathan@gmail.com>
2015-11-26 13:01:46 +0000

    user cannot write more than one review per restaurant

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,12 @@ class User < ActiveRecord::Base
 
   has_many :restaurants
   has_many :reviews
+  has_many :reviewed_restaurants, through: :reviews, source: :restaurant
+
+  def has_reviewed? restaurant
+    reviewed_restaurants.include? restaurant
+  end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -14,4 +20,5 @@ class User < ActiveRecord::Base
       user.password = Devise.friendly_token[0,20]
     end
   end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,5 @@ require 'spec_helper'
 describe User, type: :model do
   it { is_expected.to have_many :restaurants }
   it { is_expected.to have_many :reviews }
+  it { is_expected.to have_many :reviewed_restaurants }
 end
