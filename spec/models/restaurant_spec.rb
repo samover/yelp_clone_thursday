76302ba2f780e911e69d05e97a2a13b6bb8fac5f
samover <samuel.overloop@gmail.com>
2015-11-26 15:25:22 +0000

    unit test unique review per restaurant user

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,6 +1,11 @@
 require 'spec_helper'
 
 describe Restaurant, type: :model do
+  let(:user) { FactoryGirl.build(:user) }
+  let(:review_params) { { thoughts: 'So so', rating: 3 } } 
+
+  subject(:restaurant) { FactoryGirlFactoryGirl..build(:restaurant) }
+
   it { is_expected.to have_many :reviews }
 
   it 'is not valid unless it has a unique name' do
@@ -8,4 +13,12 @@ describe Restaurant, type: :model do
     restaurant = Restaurant.new(name: 'Five Guys')
     expect(restaurant).to have(1).error_on :name
   end
+
+  it 'can not have more than one review by the same user' do
+    review1 = restaurant.build_review(review_params, user)
+    review1.save
+    review2 = restaurant.build_review(review_params, user)
+    expect(review2.save).to eq false
+  end
+
 end
