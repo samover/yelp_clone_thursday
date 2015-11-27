a503436e2b4d7b72422012a67848230fde9aca8c
thisdotrob <thisdotrob@gmail.com>
2015-11-27 14:35:26 +0000

    Refactor endorsements displays

--- spec/features/endorsement_features_spec.rb
+++ spec/features/endorsement_features_spec.rb
@@ -5,13 +5,16 @@ feature 'endorsing reviews' do
     sign_in
     add_restaurant
     create_review
+    click_link 'KFC'
+    click_link 'Endorse Review'
   end
 
   it 'user can endorse a review' do
-    sign_out
-    sign_in(email: 'rob@makers.com', password: 'password')
-    click_link 'KFC'
-    click_link 'Endorse Review'
     expect(page).to have_content '1 endorsement'
+  end
+
+  it 'user cannot endors a review twice' do
+    click_link 'KFC'
+    expect(page).to_not have_link 'Endorse Review'
   end
 end
