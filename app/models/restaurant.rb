acec683b56615d7098828842a02836cc0bf69db7
samover <samuel.overloop@gmail.com>
2015-11-27 16:26:31 +0000

    add image association to restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,9 @@ class Restaurant < ActiveRecord::Base
   belongs_to :user
   has_many :reviews, dependent: :destroy
   validates :name, length: { minimum: 3 }, uniqueness: true
+  has_attached_file :image, styles: { medium: '300x300>', thumb: '100x100>' }, 
+    default_url: '/images/:style/missing.png'
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
   def build_review review_params, user
     review = reviews.new(review_params)
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151127130057) do
+ActiveRecord::Schema.define(version: 20151127162332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,11 +29,15 @@ ActiveRecord::Schema.define(version: 20151127130057) do
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
     t.integer  "rating"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.string   "location"
     t.text     "description"
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "restaurants", ["user_id"], name: "index_restaurants_on_user_id", using: :btree
